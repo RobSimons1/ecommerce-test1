6f7190d031e24c677f40fb845c5d1ab0166c1637
RobSimons1 <rob.simons79@gmail.com>
2020-03-28 20:10:53 +0000

    Added allowed host for Heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8080-fefe6f19-076d-42fb-b95b-1df5e46115d4.ws-eu01.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8080-fefe6f19-076d-42fb-b95b-1df5e46115d4.ws-eu01.gitpod.io', 'localhost', 'robs-ecommerce.herokuapp.com']
 
 
 # Application definition
