f30d8288c6166b9c2ebfd86bb6a9c576b9794bd3
RobSimons1 <rob.simons79@gmail.com>
2020-12-09 14:01:46 +0000

    Body of message added for contact email

--- accounts/views.py
+++ accounts/views.py
@@ -32,7 +32,7 @@ def contact(request):
                 request.POST['user_email'], # user email displays in subject field and can be responded to                
                 request.POST['message'], 
                 # body of message 
-                "OaklandSI", 
+                "Oakland Site Investigation", 
                 # name of company appears in from_email field to know where email has derived from          
                 ['rob.simons79@gmail.com'], # email_to recipiant 
                 fail_silently=False,
