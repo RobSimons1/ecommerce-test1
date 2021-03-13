169b6114fadf2edb6713ddbc5608d6266c1823ca
RobSimons1 <rob.simons79@gmail.com>
2021-03-13 10:50:25 +0000

    downloads views.py updated

--- downloads/views.py
+++ downloads/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def downloads(request):
+    """A view that displays the downloads page"""
+    return render(request, "downloads.html")
