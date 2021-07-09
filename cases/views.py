295294a31800eb943b83c27b7fb80d6248a383a7
RobSimons1 <rob.simons79@gmail.com>
2021-07-09 13:05:21 +0000

    Cases page hooked up to DJANGO DB to show case studies

--- cases/views.py
+++ cases/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Cases
 
 # Create your views here.
 def cases(request):
     """A view that displays the cases page"""
-    return render(request, "cases.html")
+    cases = Cases.objects.all()
+    return render(request, "cases.html", {"cases": cases})
